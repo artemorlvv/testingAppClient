ce5a1d9435d349f76155395c9376cb38264d1040
artemorlvv <artemorlvv@gmail.com>
2024-06-07 08:02:45 +0500

    udp

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import path from "path";
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const buildPath = path.resolve(__dirname, "dist"); // Абсолютный путь к папке сборки
+const buildPath = path.resolve(__dirname, "dist");
 
 app.use(express.static(buildPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(buildPath, "index.html")); // Абсолютный путь к index.html
+  res.sendFile(path.resolve(buildPath, "index.html"));
 });
 
 app.listen(PORT, () => {
