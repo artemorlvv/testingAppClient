6552372081180041b032b4550fd43a1037d3d6da
artemorlvv <artemorlvv@gmail.com>
2024-06-07 08:01:58 +0500

    aa

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(import.meta.url, "dist")));
+const buildPath = path.resolve(__dirname, "dist"); // Абсолютный путь к папке сборки
+
+app.use(express.static(buildPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(import.meta.url, "dist", "index.html"));
+  res.sendFile(path.resolve(buildPath, "index.html")); // Абсолютный путь к index.html
 });
 
 app.listen(PORT, () => {
