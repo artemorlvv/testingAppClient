6383bf0f71c665d512c70bbdf83cbaa2c4fa70d0
artemorlvv <artemorlvv@gmail.com>
2024-06-07 07:59:49 +0500

    upd

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(import.meta.url, "build")));
+app.use(express.static(path.join(import.meta.url, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(import.meta.url, "build", "index.html"));
+  res.sendFile(path.join(import.meta.url, "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
